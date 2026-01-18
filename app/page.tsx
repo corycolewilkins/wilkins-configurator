df030096780ed53139fb0a1edfe16d263d62f6c2
Cory Wilkins <corycolewilkins@hotmail.co.uk>
2026-01-18 22:00:10 +0000

    thicker door border and make white

--- app/page.tsx
+++ app/page.tsx
@@ -318,7 +318,7 @@ export default function Page() {
               <div className="mt-3 flex justify-center overflow-x-auto">
                 <div
                   className={`relative overflow-hidden rounded-2xl flex-shrink-0 ${
-                    includeExterior ? "border-4 border-neutral-600" : "border border-neutral-800"
+                    includeExterior ? "border-8 border-white" : "border border-neutral-800"
                   }`}
                   style={{ width: wallDims.wallW, height: wallDims.wallH }}
                 >
